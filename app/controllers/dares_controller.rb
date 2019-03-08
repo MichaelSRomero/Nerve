61dbaa4cbeb179a1453ca0cca1c1e90779443fe0
MichaelRomero <liikemike@aim.com>
2019-03-07 23:45:11 -0500

    added new & create paths for dare controller

--- app/controllers/dares_controller.rb
+++ app/controllers/dares_controller.rb
@@ -12,6 +12,20 @@ class DaresController < ApplicationController
   end
 
   def create
+    @dare = Dare.create(dare_params)
+
+    if @dare.valid?
+      redirect_to dares_path
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_dare_path
+    end
   end
-  
+
+  private
+
+  def dare_params
+    params.require(:dares).permit(:content, :category_id)
+  end
+
 end
--- app/models/dare.rb
+++ app/models/dare.rb
@@ -1,4 +1,6 @@
 class Dare < ApplicationRecord
   has_many :user_dares
   has_many :users, through: :dares
+
+  validations :content, presence: true, uniqueness: true
 end
